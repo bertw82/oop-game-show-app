d8eb9a907a82101bfcdb22dc19ec909916b1ab67
Bert Witzel <bwitzel82@gmail.com>
2021-05-04 20:51:25 -0400

    work on fetch API quote generator

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,38 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+    * Get phrases for use in game from random phrase API: https://github.com/lukePeavey/quotable
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        let phraseArray = [];
+        for (let i = 0; i < 5; i++) {
+            fetch('https://api.quotable.io/random?maxLength=30')
+            .then(response => response.json())
+            .then(data => {
+                const phrase = new Phrase(data.content);
+                phraseArray.push(phrase);
+            })
+            .catch(error => console.log('Looks like there was a problem!', error))
+        }
+        return phraseArray;
+    };
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase.phrase;
+    };
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+
+const phrase = new Phrase('life is like chocolate');
+const phrase1 = new Phrase('hey there');
+const phrase2 = new Phrase('blh blh blh');
+
+const newphrasearray = [phrase, phrase1, phrase2];
+console.log(newphrasearray);
+
+newphrasearray.forEach((phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+    });
+const win = new Game();
+console.log(win.phrases);
