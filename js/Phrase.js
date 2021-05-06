e3ed9a775cebce73f6721e92044db50e03bce317
Bert Witzel <bwitzel82@gmail.com>
2021-05-06 15:50:08 -0400

    update phrase.js
    complete checkLetter() and showMatchedLetter()

--- js/Phrase.js
+++ js/Phrase.js
@@ -24,6 +24,37 @@ class Phrase {
             }
             ul.appendChild(li);
         }
-
     }
-}
+
+    /**
+    * Checks if passed letter is in phrase
+    * @param (string) letter - Letter to check
+    */
+    checkLetter(letter) {
+        const currentPhrase = this.phrase;
+        const currentPhraseArray = currentPhrase.split('');
+        const result = currentPhraseArray.filter(char => char === letter);
+        if (result.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    /**
+    * Displays passed letter on screen after a match is found
+    * @param (string) letter - Letter to display
+    */
+    showMatchedLetter(letter) {
+        if (this.checkLetter(letter)) {
+            console.log('hello');
+            const phraseLi = document.querySelectorAll('#phrase ul li');
+            for (let i = 0; i < phraseLi.length; i++) {
+                if (phraseLi[i].classList.contains(letter)){
+                    phraseLi[i].classList.remove('hide');
+                    phraseLi[i].classList.add('show');
+                }
+            }
+        }
+    }
+};
